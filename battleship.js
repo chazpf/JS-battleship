a95227b551de3db756848c9faeb66aea7d4a2c83
chazpf <74632592+chazpf@users.noreply.github.com>
2020-11-29 02:22:52 -0600

    Add random start locations

--- battleship.js
+++ battleship.js
@@ -1,8 +1,10 @@
 //Declare initial variables
-let location1 = 3;
-let location2 = 4;
-let location3 = 5;
+let randomLoc = Math.floor(Math.random() * 5);
+let location1 = randomLoc;
+let location2 = location1 + 1;
+let location3 = location2 + 1;
 let guess;
+let alreadyGuessed = [];
 let numHits = 0;
 let numGuesses = 0;
 let isSunk = false;
@@ -10,19 +12,24 @@ let isSunk = false;
 //Loop while the ship is not sunk
 while (isSunk == false) {
 	guess = prompt('Ready, aim, fire! (enter a number from 0-6):');
-	if (guess < 0 || guess > 6) {
-		alert('Please enter a valid cell number!');
+	if (alreadyGuessed.includes(guess)) {
+		alert(`You've already guessed that number! Please try again.`);
 	} else {
-		numGuesses += 1;
-		if (guess == location1 || guess == location2 || guess ==location3) {
-			alert('HIT!');
-			numHits += 1;
-			if (numHits == 3) {
-				isSunk = true;
-				alert('You sunk my battleship!');
-			}
+		if (guess < 0 || guess > 6) {
+			alert('Please enter a valid cell number!');
 		} else {
-			alert('MISS!');
+			alreadyGuessed.push(guess);
+			numGuesses += 1;
+			if (guess == location1 || guess == location2 || guess ==location3) {
+				alert('HIT!');
+				numHits += 1;
+				if (numHits == 3) {
+					isSunk = true;
+					alert('You sunk my battleship!');
+				}
+			} else {
+				alert('MISS!');
+			}
 		}
 	}
 }
